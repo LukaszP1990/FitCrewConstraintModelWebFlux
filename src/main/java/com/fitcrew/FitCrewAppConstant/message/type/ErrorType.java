588655fe238e903a386717cc34dca8370457039e
lptaszek-cl <lptaszek@corelogic.com>
2021-01-29 09:00:35 +0100

    update error msg

--- src/main/java/com/fitcrew/FitCrewAppConstant/message/type/ErrorType.java
+++ src/main/java/com/fitcrew/FitCrewAppConstant/message/type/ErrorType.java
@@ -3,7 +3,6 @@ package com.fitcrew.FitCrewAppConstant.message.type;
 public enum ErrorType {
 
 	NO_CLIENT_BOUGHT_TRAINING("No client bought training"),
-	NOT_SUCCESSFULLY_MAPPING("Client object not mapped successfully"),
 	NO_CLIENT_FOUND("No client found"),
 	NO_CLIENT_DELETED("No client deleted"),
 	NO_CLIENT_UPDATED("No client updated"),
@@ -13,12 +12,10 @@ public enum ErrorType {
 	NO_TRAINERS_SORTED("No trainers sorted"),
 	NO_EMAIL_SENT("Email sending failed"),
 	NO_CLIENT_SAVED("Client save failed"),
-	UNAUTHORIZED("Client is unauthorized"),
 	LOGIN_ERROR("Login failed"),
 	NO_TRAINER_DELETED("No trainer deleted"),
 	NO_TRAINER_UPDATED("No trainer updated"),
 	NO_TRAINERS("No trainers found"),
-	NO_TRAINER("No trainer found"),
 	NO_TRAININGS("No trainings found"),
 	NO_TRAINER_SAVED("Trainer save failed"),
 	NO_TRAINING_SELECTED("No trainings selected"),
@@ -28,7 +25,9 @@ public enum ErrorType {
 	NO_ADMIN_FOUND("No admin found"),
 	NO_ADMIN_SAVED("Admin save failed"),
 	NO_CLIENTS_FOUND("No clients found"),
-	NO_TRAININGS_BOUGHT("No trainings has client bought");
+	NO_TRAINER_BY_TRAINER_ID("No trainer found by trainer id"),
+	NO_CLIENT_BY_TRAINER_ID("No client found by trainer id"),
+	NO_ADMIN_BY_TRAINER_ID("No admin found by trainer id");
 
 	private final String text;
 
